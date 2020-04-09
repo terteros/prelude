45a655b8138a58161e5fffabe32a4df209ba6056
batuhan <Batuhan Karagoz>
2020-04-09 20:29:43 +0300

    neotree

--- personal/my-config.el
+++ personal/my-config.el
@@ -21,7 +21,9 @@
     material-theme
     spacemacs-theme
     spaceline
-    py-autopep8))
+    py-autopep8
+    neotree
+    ))
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
@@ -32,9 +34,9 @@
 ;; --------------------------------------
 
 (setq inhibit-startup-message t) ;; hide the startup message
-;(load-theme 'solarized-dark t) ;; load material theme
-;(load-theme 'spacemacs-theme t) ;; load material theme
-(load-theme 'spacemacs-dark t)
+(load-theme 'solarized-dark t)
+;(load-theme 'spacemacs-theme t)
+;(load-theme 'spacemacs-dark t)
 (require 'spaceline-config)
 (spaceline-emacs-theme)
 
@@ -59,7 +61,7 @@
 (setenv "WORKON_HOME" "/home/batuhan/anaconda3/envs")
 ;(setq elpy-rpc-python-command "/home/batuhan/anaconda3/bin/python")
 (pyvenv-mode 1)
-(pyvenv-activate "/home/batuhan/anaconda3/envs/spin")
+(pyvenv-activate "/home/batuhan/anaconda3/envs/bmenv")
 
 (elpy-enable)
 
@@ -94,18 +96,43 @@
             (concat "\\(def\\|class\\)\s" (thing-at-point 'symbol) "(")))))
 (define-key elpy-mode-map (kbd "M-.") 'elpy-goto-definition-or-rgrep)
 
-;;sp-convolute-sexp
+;; use rgrep when xref-find-references fails
+(defun xref-find-references-or-rgrep ()
+  "xref-find-references for the symbol at point, if found. Otherwise, run `elpy-rgrep-symbol'."
+  (interactive)
+  (ring-insert find-tag-marker-ring (point-marker))
+  (condition-case nil (xref-find-references)
+    (error (elpy-rgrep-symbol
+            (thing-at-point 'symbol)))))
+;;(define-key elpy-mode-map (kbd "M-?") 'xref-find-references-or-rgrep)
+
+
+;;fix sp-convolute-sexp conflicting with xref-find-references
 (custom-set-variables
  '(sp-override-key-bindings (quote (("M-?")))))
 
 ;;MAGIT
-(global-set-key (kbd "C-x g") 'magit-status)
+;(global-set-key (kbd "C-x g") 'magit-status)
 
 ;;RAZER BINDING
 (global-set-key [XF86Tools] 'other-window) ;C-x o
 (global-set-key [XF86Launch5] 'other-frame) ;C-x 5 o
 (global-set-key [XF86Launch6] 'comment-region)
 (global-set-key [XF86Launch7] 'uncomment-region)
+(global-set-key [XF86Launch8] 'projectile-find-file)
+
+;; Toggle window dedication
+(defun toggle-window-dedicated ()
+  "Toggle whether the current active window is dedicated or not"
+  (interactive)
+  (message
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window
+                                 (not (window-dedicated-p window))))
+       "Window '%s' is dedicated"
+     "Window '%s' is normal")
+   (current-buffer)))
+
 
 
 ;; ;;AUCTEX
@@ -113,17 +140,21 @@
 ;; (setq TeX-parse-self t)
 ;; (setq-default TeX-master nil)
 
-;; (defun respy ()
-;;   "Restart python console before evaluate buffer or region to avoid various uncanny conflicts, like not reloding modules even when they are changed"
-;;   (interactive)
-;;   (kill-process "Python")
-;;   (sleep-for 0.1)
-;;   (kill-buffer "*Python*")
-;;   (elpy-shell-send-region-or-buffer))
+(defun respy ()
+  "Restart python console before evaluate buffer or region to avoid various uncanny conflicts, like not reloding modules even when they are changed"
+  (interactive)
+  (kill-process "Python")
+  (sleep-for 0.1)
+  (kill-buffer "*Python*")
+  (elpy-shell-send-region-or-buffer))
 
 ;;UTF-8
 (define-coding-system-alias 'UTF-8 'utf-8)
 
 
 (put 'dired-find-alternate-file 'disabled nil)
+
+;;NEOTREE
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
 ;; init.el ends here
